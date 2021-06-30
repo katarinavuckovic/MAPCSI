c6c5a62769e587f4cec9a2a081cf190dfd1a8f88
katarinavuckovic <71682568+katarinavuckovic@users.noreply.github.com>
2021-06-30 16:48:32 -0400

    Update main_DatasetGenerator.m

--- Dataset/main_DatasetGenerator.m
+++ Dataset/main_DatasetGenerator.m
@@ -6,7 +6,6 @@ clc
 %Open Data_Generator to adjust number of ULA antennas (Nt) and number of
 %subcarriers (Nc)
 [Dataset,params]= Dataset_Generator('colo_direct'); 
-f = 'D:/MATLAB/data_generation_package/data_generation_package/MAT functions/'; %location of 
 
 % Adjust based on ULA antennas
 Nt = 60;
